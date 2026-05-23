3f0197d0e9a2349e05cdfe5a7ae4d3886ffa7d9b
Nicolas-Setien <nasetien.20@est.ucab.edu.ve>
2026-05-22 20:11:10 -0500

    Photo shuffler function

--- script.js
+++ script.js
@@ -1,6 +1,69 @@
-topImg = document.querySelector(".top-img");
-midImg = document.querySelector(".mid-img");
-lowImg = document.querySelector(".low-img");
+topImg = document.querySelectorAll(".top-img");
+midImg = document.querySelectorAll(".mid-img");
+lowImg = document.querySelectorAll(".low-img");
+
+topImg.title = "Click me to see more!";
+// topImg.style.transform = "rotate(80deg)";
+console.log(topImg);
+
+for (var i in topImg) {
+    if (topImg[i] instanceof Node) {
+        topImg[i].title = "Click me to see more!";
+        topImg[i].addEventListener("click", shufflePhotosTop.bind(this, i));
+    }
+}
+
+for (var i in midImg) {
+    if (midImg[i] instanceof Node) {
+        midImg[i].addEventListener("click", shufflePhotosMid.bind(this, i));
+    }
+}
+
+for (var i in lowImg) {
+    if (lowImg[i] instanceof Node) {
+        lowImg[i].addEventListener("click", shufflePhotosLow.bind(this, i));
+    }
+}
+
+function shufflePhotosTop(index){
+    midImg[index].style.zIndex = "0";
+    midImg[index].style.transform = "rotate(0deg)";
+
+    lowImg[index].style.zIndex = "-1";
+    lowImg[index].style.transform =
+        `rotate(${Math.floor(Math.random() * 10) - 5}deg)`;
+
+    topImg[index].style.zIndex = "-2";
+    topImg[index].style.transform = 
+        `rotate(${Math.floor(Math.random() * 10) - 5}deg)`;
+}
+
+function shufflePhotosMid(index){
+    lowImg[index].style.zIndex = "0";
+    lowImg[index].style.transform = "rotate(0deg)";
+
+    topImg[index].style.zIndex = "-1";
+    topImg[index].style.transform =
+        `rotate(${Math.floor(Math.random() * 10) - 5}deg)`;
+
+    midImg[index].style.zIndex = "-2";
+    midImg[index].style.transform = 
+        `rotate(${Math.floor(Math.random() * 10) - 5}deg)`;
+}
+
+function shufflePhotosLow(index){
+    topImg[index].style.zIndex = "0";
+    topImg[index].style.transform = "rotate(0deg)";
+
+    midImg[index].style.zIndex = "-1";
+    midImg[index].style.transform =
+        `rotate(${Math.floor(Math.random() * 10) - 5}deg)`;
+
+    lowImg[index].style.zIndex = "-2";
+    lowImg[index].style.transform = 
+        `rotate(${Math.floor(Math.random() * 10) - 5}deg)`;
+}
+/*
 topImg.addEventListener("click", () => {
     midImg.style.zIndex = "0";
     midImg.style.transform = "rotate(0deg)";
@@ -11,26 +74,4 @@ topImg.addEventListener("click", () => {
     topImg.style.zIndex = "-2";
     topImg.style.transform = "rotate(-4deg)";
 });
-
-midImg.addEventListener("click", () => {
-    lowImg.style.zIndex = "0";
-    lowImg.style.transform = "rotate(0deg)";
-
-    topImg.style.zIndex = "-1";
-    topImg.style.transform = "rotate(3deg)";
-
-    midImg.style.zIndex = "-2";
-    midImg.style.transform = "rotate(-2deg)";
-});
-
-lowImg.addEventListener("click", () => {
-    topImg.style.zIndex = "0";
-    topImg.style.transform = "rotate(0deg)";
-
-    midImg.style.zIndex = "-1";
-    midImg.style.transform = "rotate(4deg)";
-
-    lowImg.style.zIndex = "-2";
-    lowImg.style.transform = "rotate(-6deg)";
-
-});
+*/
